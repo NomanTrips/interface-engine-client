40cc252aa5f2bb646d7d1bc80f3d8aaed68644a5
Brian M. Scott <brianscott0017@yahoo.com>
2018-05-02 21:53:49 -0400

    re-did sidenav  upgraded vuetify

--- src/components/Channel.vue
+++ src/components/Channel.vue
@@ -1,17 +1,96 @@
 <template>
   <div>
-    <v-navigation-drawer app clipped fixed width="200"  v-model="drawer">
-      <v-list>   
-        <v-list-tile v-for="item in navitems" :key="item.title" @click="navItem(item.title)">
-          <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>      
+  <v-navigation-drawer app clipped fixed  width="200" v-model="drawer"
+    stateless
+    value="true"
+  >
+    <v-list>
+      <v-list-group
+        append-icon="settings"
+        value="true"
+      >
+        <v-list-tile slot="activator" @click="navItem('Settings')">
+          <v-list-tile-title>Settings</v-list-tile-title>
+        </v-list-tile>
+        <v-list-group
+          sub-group
+          no-action
+        >
+          <v-list-tile slot="activator">
+            <v-list-tile-title>Source</v-list-tile-title>
+          </v-list-tile>
+        </v-list-group>
+        
+        <v-list-group
+          sub-group
+          no-action
+    
+        >        
+          <v-list-tile slot="activator">
+            <v-list-tile-title>Destination</v-list-tile-title>
+          </v-list-tile>
+        </v-list-group>
+        
+      </v-list-group>
+
+      <v-list-group
+        append-icon="message"
+        value="true"
+      >
+        <v-list-tile slot="activator" @click="navItem('Messages')">
+          <v-list-tile-title>Messages</v-list-tile-title>
+        </v-list-tile>
+           <v-list-tile v-show="false">
+            <v-list-tile-title class="pl-3">Send message</v-list-tile-title>
+          </v-list-tile>
+
+<!--
+        <v-list-group
+          sub-group
+          no-action
+          value="false"
+        >
+
+        </v-list-group>
+-->
+      </v-list-group>
+
+      <v-list-group
+        append-icon="code"
+        value="true"
+      >
+        <v-list-tile slot="activator" @click="navItem('Scripts')">
+          <v-list-tile-title>Scripts</v-list-tile-title>
+        </v-list-tile>
+
+      </v-list-group>
+
+    </v-list>
+  </v-navigation-drawer>
+<!--
+    <v-navigation-drawer app clipped fixed  v-model="drawer">
+      <v-list>
+     <v-list-group value="true"> 
+          <v-list-tile v-for="item in navitems" :key="item.title" >
+              <v-list-tile-title >{{ item.title }}</v-list-tile-title>
+              <v-list-tile-action>
+                <v-icon >{{ item.icon }}</v-icon>
+              </v-list-tile-action>
+           </v-list-tile>
+
+           <v-list-group sub-group no-action value="true">
+                    
+            <v-list-tile  v-for="subitem in subitems" :key="subitem.title" @click="navItem(subitem.title)" >
+              <v-list-tile-title >{{ subitem.title }}</v-list-tile-title>
+              <v-list-tile-action>
+                <v-icon >{{ subitem.icon }}</v-icon>
+              </v-list-tile-action>
+            </v-list-tile>
+            </v-list-group>   
+</v-list-group>
       </v-list>
     </v-navigation-drawer>
+-->
     <router-view></router-view>
 
 
@@ -23,12 +102,25 @@ export default {
   name: 'channel',
   data() {
     return {
+            admins: [
+        ['Management', 'people_outline'],
+        ['Settings', 'settings']
+      ],
+      cruds: [
+        ['Create', 'add'],
+        ['Read', 'insert_drive_file'],
+        ['Update', 'update'],
+        ['Delete', 'delete']
+      ],
       homePath: '/',
       navitems: [
         { icon: 'settings', title: 'Settings' },
         { icon: 'message', title: 'Messages'  },
-        { icon: 'code', title: 'Scripts'  },
-        { icon: 'storage', title: 'Storage'  },
+        { icon: 'code', title: 'Scripts',   },
+        { icon: 'storage', title: 'Storage', },
+      ],
+      subitems: [
+        { icon: 'send', title: 'Send message' },
       ],
       clipped: true,
       drawer: true,
