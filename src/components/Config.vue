c51bdd505fd015e52158fb18959c20b0adab50e7
Brian M. Scott <brianscott0017@yahoo.com>
2018-05-14 21:25:30 -0400

    added tooltip regarindg variable formats

--- src/components/Config.vue
+++ src/components/Config.vue
@@ -106,7 +106,7 @@
             <v-switch v-bind:label="'Use post-process query:'" v-model="channel.db_reader_use_post_process_query" ></v-switch>
             <v-text-field
               name="input-7-1"
-              label="Post processing query:"
+              label="Post processing query (enter variables as: $$'+`${id}`+'$$):"
               multi-line
               v-model="channel.db_reader_post_process_query"
               rows="10"
